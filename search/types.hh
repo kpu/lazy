191614ae26dc8a3445a230d1c08718485b193dec
Kenneth Heafield <github@kheafield.com>
2012-01-24 13:15:57 -0500

    First pass at writing a vertex (max over edges).  Probably doesn't compile.

---
+++ search/types.hh
@@ -0,0 +1,14 @@
+#ifndef SEARCH_TYPES__
+#define SEARCH_TYPES__
+
+#include <cmath>
+
+namespace search {
+
+typedef unsigned int Index;
+typedef float Score;
+const Score kScoreInf = INFINITY;
+
+} // namespace search
+
+#endif // SEARCH_TYPES__
---
+++ search/vertex.hh
@@ -0,0 +1,118 @@
+#ifndef SEARCH_VERTEX__
+#define SEARCH_VERTEX__
+
+#include "search/types.hh"
+
+#include <queue>
+#include <vector>
+
+namespace search {
+
+class Edge;
+
+template <class Final> class Vertex {
+  private:
+    struct QueueEntry {
+      Edge *edge;
+      Index index;
+      // Cached score.  
+      Score score;
+      // Priority queue's order is annoying.  
+      bool operator<(const QueueEntry &other) const {
+        return score > other.entry;
+      }
+    };
+
+  public:
+    Vertex() : bound_(kScoreInf) {}
+
+    void AddEdge(Edge *edge) {
+      QueueEntry entry;
+      entry.edge = edge;
+      entry.index = 0;
+      entry.score = kScoreInf;
+      edges_.push(entry);
+    }
+
+    Index Size() const {
+      return final_.size();
+    }
+
+    const Final &operator[](Index i) const {
+      return *final_[i];
+    }
+
+    Score Bound() const {
+      return bound_;
+    }
+
+    void More(const Score beat) {
+      if (bound_ < beat) return;
+      while (!edges_.empty()) {
+        QueueEntry top(edges_.top());
+        if (top.score < beat) {
+          bound_ = top.score;
+          return;
+        }
+        edges_.pop();
+        Consider(beat, top);
+      }
+      bound_ = -kScoreInf;
+    }
+
+  private:
+    void Consider(const Score beat, QueueEntry &top) {
+      Edge &edge = *top.edge;
+      if (edge.Size() > top.index) {
+        // Have a concrete hypothesis.
+        const Final &got = edge[top.index];
+        if (top.score != got.Score()) {
+          top.score = got.Score();
+          edges_.push(top);
+          return;
+        }
+        // Hypothesis matches the cached score.  Use it.  
+        final_.push_back(&got);
+        ++top.index;
+        PushLower(top);
+        return;
+      }
+      // No concrete hypothesis.
+      if (top.score != edge.Bound()) {
+        PushBound(top);
+        return;
+      }
+      Score to_beat;
+      if (edges_.empty()) {
+        to_beat = beat;
+      } else {
+        to_beat = std::max(beat, edges_.top().score);
+      }
+      edge.More(to_beat);
+      PushLower(top);
+    }
+
+    void PushLower(QueueEntry &entry) {
+      const Edge &edge = *entry.edge;
+      if (edge.Size() > entry.index) {
+        entry.score = edge[entry.index].Score();
+        edges_.push(entry);
+        return;
+      }
+      PushBound(entry);
+    }
+
+    void PushBound(QueueEntry &entry) {
+      entry.score = entry.edge->Bound();
+      if (entry.score != -kScoreInf) edges_.push(entry);
+    }
+
+    std::vector<const Final*> final_;
+
+    Score bound_;
+
+    std::priority_queue<QueueEntry> edges_;
+};
+
+} // namespace search
+#endif // SEARCH_VERTEX__
