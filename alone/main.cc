167e1f5ea45d66cc95eaed86398f5fbe230ca37d
Kenneth Heafield <kenlm@kheafield.com>
2012-02-28 21:49:00 -0500

    Saner progress bars

--- alone/main.cc
+++ alone/main.cc
@@ -5,9 +5,10 @@
 
 namespace alone {
 
-void Decode(const char *lm, util::FilePiece &graph_file, StringPiece weights) {
+void Decode(const char *lm, const char *graph_name, StringPiece weights) {
   Context context(lm, weights);
   Graph graph;
+  util::FilePiece graph_file(graph_name, &std::cerr);
   ReadCDec(context, graph_file, graph);
 }
 
@@ -18,7 +19,6 @@ int main(int argc, char *argv[]) {
     std::cerr << argv[0] << " lm graph \"weights\"" << std::endl;
     return 1;
   }
-  util::FilePiece graph(argv[2], &std::cerr);
-  alone::Decode(argv[1], graph, argv[3]);
+  alone::Decode(argv[1], argv[2], argv[3]);
   return 0;
 }
