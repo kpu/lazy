8da568ef0b4484d77e4ff668a8aaa8456c386a7c
Kenneth Heafield <github@kheafield.com>
2012-02-29 16:39:24 -0800

    Read all vocab

--- alone/just_vocab.cc
+++ alone/just_vocab.cc
@@ -5,5 +5,10 @@
 
 int main() {
   util::FilePiece f(0, "stdin", &std::cerr);
-  alone::JustVocab(f, std::cout);
+  while (true) {
+    try {
+      alone::JustVocab(f, std::cout);
+    } catch (const util::EndOfFileException &e) { break; }
+    std::cout << '\n';
+  }
 }
