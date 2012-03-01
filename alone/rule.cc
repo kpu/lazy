5a1dd9d73fc42dbfca89d750ff3aa5a27aff983f
Kenneth Heafield <kheafiel@cluster01.lti.ece.cmu.local>
2012-02-29 21:49:35 -0500

    Get paranoid and slow about positive backoffs

--- alone/rule.cc
+++ alone/rule.cc
@@ -5,14 +5,22 @@
 
 namespace alone {
 
+float PositiveBackoffs(const lm::ngram::ChartState &state) {
+  float ret = 0.0;
+  for (unsigned char i = 0; i < state.right.length; ++i) {
+    if (state.right.backoff[i] > 0.0) ret+= state.right.backoff[i];
+  }
+  return ret;
+}
+
 void Rule::FinishedAdding(const Context &context, search::Score additive, bool bos) {
   additive_ = additive;
   bos_ = bos;
   search::Score lm_score = 0.0;
-  lm::ngram::ChartState ignored;
+  lm::ngram::ChartState state;
   const lm::WordIndex oov = context.LanguageModel().GetVocabulary().NotFound();
   for (std::vector<Word>::const_iterator word = items_.begin(); ; ++word) {
-    lm::ngram::RuleScore<lm::ngram::RestProbingModel> scorer(context.LanguageModel(), ignored);
+    lm::ngram::RuleScore<lm::ngram::RestProbingModel> scorer(context.LanguageModel(), state);
     for (; ; ++word) {
       if (word == items_.end()) {
         bound_ = additive_ + context.GetWeights().LM() * lm_score;
@@ -23,6 +31,7 @@ void Rule::FinishedAdding(const Context &context, search::Score additive, bool b
       scorer.Terminal(word->Index());
     }
     lm_score += scorer.Finish();
+    lm_score += PositiveBackoffs(state);
   }
 }
 
@@ -35,12 +44,14 @@ search::Score Rule::Apply(const Context &context, const std::vector<const Final 
     if (i->Terminal()) {
       scorer.Terminal(i->Index());
     } else {
-      scorer.NonTerminal((*child)->State(), 0.0);
+      scorer.NonTerminal((*child)->State(), -PositiveBackoffs((*child)->State()));
       ret += (*child)->Total();
       ++child;
     }
   }
-  return ret + context.GetWeights().LM() * scorer.Finish();
+  float lm_score = scorer.Finish();
+  lm_score += PositiveBackoffs(state);
+  return ret + context.GetWeights().LM() * lm_score;
 }
 
 } // namespace alone
