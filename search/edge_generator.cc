1edc8ceb32ca5b58fc48f3f9bcbbc4ea5878d9c6
Kenneth Heafield <kheafiel@cluster01.lti.ece.cmu.local>
2012-05-28 23:04:01 -0400

    Change to incremental scoring.

--- search/edge_generator.cc
+++ search/edge_generator.cc
@@ -6,6 +6,8 @@
 #include "search/vertex.hh"
 #include "search/vertex_generator.hh"
 
+#include <numeric>
+
 namespace search {
 
 bool EdgeGenerator::Init(Edge &edge) {
@@ -49,14 +51,17 @@ template <class Model> float FastScore(const Context<Model> &context, unsigned c
   const lm::ngram::ChartState &previous_reveal = previous.nt[victim].State();
   const PartialVertex &update_nt = update.nt[victim];
   const lm::ngram::ChartState &update_reveal = update_nt.State();
-  if (update_reveal.left.length > previous_reveal.left.length || (update_reveal.left.full && !previous_reveal.left.full)) {
-    ret += lm::ngram::RevealAfter(context.LanguageModel(), before->left, before->right, update_reveal.left, previous_reveal.left.length);
+  float just_after = 0.0;
+  if ((update_reveal.left.length > previous_reveal.left.length) || (update_reveal.left.full && !previous_reveal.left.full)) {
+    just_after += lm::ngram::RevealAfter(context.LanguageModel(), before->left, before->right, update_reveal.left, previous_reveal.left.length);
   }
-  if (update_reveal.right.length > previous_reveal.right.length || (update_nt.RightFull() && !previous.nt[victim].RightFull())) {
+  if ((update_reveal.right.length > previous_reveal.right.length) || (update_nt.RightFull() && !previous.nt[victim].RightFull())) {
     ret += lm::ngram::RevealBefore(context.LanguageModel(), update_reveal.right, previous_reveal.right.length, update_nt.RightFull(), after->left, after->right);
   }
   if (update_nt.Complete()) {
-    if (!update_reveal.left.full) {
+    if (update_reveal.left.full) {
+      before->left.full = true;
+    } else {
       assert(update_reveal.left.length == update_reveal.right.length);
       ret += lm::ngram::Subsume(context.LanguageModel(), before->left, before->right, after->left, after->right, update_reveal.left.length);
     }
@@ -66,7 +71,7 @@ template <class Model> float FastScore(const Context<Model> &context, unsigned c
       update.between[2].left = before->left;
     }
   }
-  return previous.score + ret * context.GetWeights().LM();
+  return previous.score + (ret + just_after) * context.GetWeights().LM();
 }
 
 } // namespace
@@ -85,7 +90,8 @@ template <class Model> bool EdgeGenerator::Pop(Context<Model> &context, VertexGe
   if (lowest_length == 255) {
     // All states report complete.  
     lm::ngram::ChartState state;
-    RecomputeFinal(context, top, state);
+    state.left = top.between[0].left;
+    state.right = top.between[GetRule().Arity()].right;
     parent.NewHypothesis(state, *from_, top);
     generate_.pop();
     top_ = generate_.empty() ? -kScoreInf : generate_.top().score;
@@ -98,16 +104,8 @@ template <class Model> bool EdgeGenerator::Pop(Context<Model> &context, VertexGe
   bool split = top.nt[victim].Split(continuation.nt[victim], alternate.nt[victim]);
 
   continuation.nt[stay] = top.nt[stay];
-  float test_score = FastScore(context, victim, GetRule().Arity(), top, continuation);
+  continuation.score = FastScore(context, victim, GetRule().Arity(), top, continuation);
 
-  continuation.score = GetRule().Bound() + Adjustment(context, continuation) * context.GetWeights().LM();
-  for (unsigned int i = 0; i < GetRule().Arity(); ++i) {
-    continuation.score += continuation.nt[i].Bound();
-  }
-  if (fabs(continuation.score - test_score) > 0.01) {
-    abort();
-  }
- 
   if (split) {
     // We have an alternate.  
     alternate.score = top.score - top.nt[victim].Bound() + alternate.nt[victim].Bound();
@@ -131,59 +129,7 @@ template <class Model> bool EdgeGenerator::Pop(Context<Model> &context, VertexGe
   return true;
 }
 
-template <class Model> void EdgeGenerator::RecomputeFinal(Context<Model> &context, const PartialEdge &to, lm::ngram::ChartState &state) {
-  if (GetRule().Arity() == 0) {
-    state = GetRule().Lexical(0);
-    return;
-  }
-  lm::ngram::RuleScore<Model> scorer(context.LanguageModel(), state);
-  if (GetRule().BeginSentence()) {
-    scorer.BeginSentence();
-    scorer.NonTerminal(GetRule().Lexical(0));
-  } else {
-    scorer.BeginNonTerminal(GetRule().Lexical(0));
-  }
-  scorer.NonTerminal(to.nt[0].State());
-  scorer.NonTerminal(GetRule().Lexical(1));
-  if (GetRule().Arity() == 2) {
-    scorer.NonTerminal(to.nt[1].State());
-    scorer.NonTerminal(GetRule().Lexical(2));
-  }
-  scorer.Finish();
-  return;
-}
-
-// TODO: this can be done WAY more efficiently.
-template <class Model> Score EdgeGenerator::Adjustment(Context<Model> &context, const PartialEdge &to) const {
-  if (GetRule().Arity() == 0) return 0.0;
-  lm::ngram::ChartState state;
-  lm::ngram::RuleScore<Model> scorer(context.LanguageModel(), state);
-  scorer.BeginNonTerminal(GetRule().Lexical(0));
-  scorer.NonTerminal(to.nt[0].State());
-  float total = 0.0;
-  if (!to.nt[0].Complete()) {
-    total += scorer.Finish();
-    scorer.Reset();
-    scorer.BeginNonTerminal(to.nt[0].State());
-  }
-  scorer.NonTerminal(GetRule().Lexical(1));
-  if (GetRule().Arity() == 1) return total + scorer.Finish();
-  assert(GetRule().Arity() == 2);
-  scorer.NonTerminal(to.nt[1].State());
-  if (!to.nt[1].Complete()) {
-    total += scorer.Finish();
-    scorer.Reset();
-    scorer.BeginNonTerminal(to.nt[1].State());
-  }
-  scorer.NonTerminal(GetRule().Lexical(2));
-  return total + scorer.Finish();
-}
-
 template bool EdgeGenerator::Pop(Context<lm::ngram::RestProbingModel> &context, VertexGenerator &parent);
 template bool EdgeGenerator::Pop(Context<lm::ngram::ProbingModel> &context, VertexGenerator &parent);
-template void EdgeGenerator::RecomputeFinal(Context<lm::ngram::RestProbingModel> &context, const PartialEdge &to, lm::ngram::ChartState &state);
-template void EdgeGenerator::RecomputeFinal(Context<lm::ngram::ProbingModel> &context, const PartialEdge &to, lm::ngram::ChartState &state);
-template Score EdgeGenerator::Adjustment(Context<lm::ngram::RestProbingModel> &context, const PartialEdge &to) const;
-template Score EdgeGenerator::Adjustment(Context<lm::ngram::ProbingModel> &context, const PartialEdge &to) const;
 
 } // namespace search
