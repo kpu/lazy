a9e4ba5c51a7627973b03a1b677c5f0610f24bc7
Kenneth Heafield <kheafiel@cluster01.lti.ece.cmu.local>
2012-05-28 21:50:00 -0400

    Easy printing from gdb

--- alone/assemble.cc
+++ alone/assemble.cc
@@ -3,6 +3,8 @@
 #include "search/final.hh"
 #include "search/rule.hh"
 
+#include <iostream>
+
 namespace alone {
 
 std::ostream &operator<<(std::ostream &o, const search::Final &final) {
@@ -54,6 +56,10 @@ void DetailedFinalInternal(std::ostream &o, const search::Final &final, const ch
 
 void DetailedFinal(std::ostream &o, const search::Final &final, const char *indent_str) {
   DetailedFinalInternal(o, final, indent_str, 0);
+}
+
+void PrintFinal(const search::Final &final) {
+  std::cout << final << std::endl;
 }
 
 } // namespace alone
