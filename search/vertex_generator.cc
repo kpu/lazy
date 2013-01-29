3a620a92f6cb82c4a485922582a659d523eedfec
Kenneth Heafield <github@kheafield.com>
2013-01-29 14:45:31 +0000

    Mild change to FindOrAdd interface

--- search/vertex_generator.cc
+++ search/vertex_generator.cc
@@ -16,8 +16,8 @@ namespace {
 
 const uint64_t kCompleteAdd = static_cast<uint64_t>(-1);
 
-Trie &FindOrInsert(ContextBase &context, Trie &node, uint64_t added, const lm::ngram::ChartState &state, unsigned char left, bool left_full, unsigned char right, bool right_full) {
-  Trie &next = node.extend[added];
+void FindOrInsert(ContextBase &context, Trie *&node, uint64_t added, const lm::ngram::ChartState &state, unsigned char left, bool left_full, unsigned char right, bool right_full) {
+  Trie &next = node->extend[added];
   if (!next.under) {
     next.under = context.NewVertexNode();
     lm::ngram::ChartState &writing = next.under->MutableState();
@@ -26,9 +26,9 @@ Trie &FindOrInsert(ContextBase &context, Trie &node, uint64_t added, const lm::n
     next.under->MutableRightFull() = right_full && state.left.full;
     writing.left.length = left;
     writing.right.length = right;
-    node.under->AddExtend(next.under);
+    node->under->AddExtend(next.under);
   }
-  return next;
+  node = &next;
 }
 
 } // namespace
@@ -40,26 +40,26 @@ void AddHypothesis(ContextBase &context, Trie &root, const NBestComplete &end) {
   Trie *node = &root;
   while (true) {
     if (left == state.left.length) {
-      node = &FindOrInsert(context, *node, kCompleteAdd - state.left.full, state, left, true, right, false);
+      FindOrInsert(context, node, kCompleteAdd - state.left.full, state, left, true, right, false);
       for (; right < state.right.length; ++right) {
-        node = &FindOrInsert(context, *node, state.right.words[right], state, left, true, right + 1, false);
+        FindOrInsert(context, node, state.right.words[right], state, left, true, right + 1, false);
       }
       break;
     }
-    node = &FindOrInsert(context, *node, state.left.pointers[left], state, left + 1, false, right, false);
+    FindOrInsert(context, node, state.left.pointers[left], state, left + 1, false, right, false);
     left++;
     if (right == state.right.length) {
-      node = &FindOrInsert(context, *node, kCompleteAdd - state.left.full, state, left, false, right, true);
+      FindOrInsert(context, node, kCompleteAdd - state.left.full, state, left, false, right, true);
       for (; left < state.left.length; ++left) {
-        node = &FindOrInsert(context, *node, state.left.pointers[left], state, left + 1, false, right, true);
+        FindOrInsert(context, node, state.left.pointers[left], state, left + 1, false, right, true);
       }
       break;
     }
-    node = &FindOrInsert(context, *node, state.right.words[right], state, left, false, right + 1, false);
+    FindOrInsert(context, node, state.right.words[right], state, left, false, right + 1, false);
     right++;
   }
 
-  node = &FindOrInsert(context, *node, kCompleteAdd - state.left.full, state, state.left.length, true, state.right.length, true);
+  FindOrInsert(context, node, kCompleteAdd - state.left.full, state, state.left.length, true, state.right.length, true);
   node->under->SetEnd(end.history, end.score);
 }
 
